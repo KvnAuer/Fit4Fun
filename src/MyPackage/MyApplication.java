5ec0c68fdc1ec16cff292cbe1f82b5a480b4ad9c
KvnAuer <kza0067@auburn.edu>
2021-01-17 15:55:28 -0600

    Started project in eclipse and this is my first commit of the project. Making sure everything is set up correctly

---
+++ src/MyPackage/MyApplication.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my main method");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module fit4Fun {
+}
